d9f29fdf85c81f88e046e1c104ebcfdba76e28a3
Kewin Warzecha <kewinwarzecha@gmail.com>
2019-05-31 21:57:54 +0200

    added database and some DB functions

---
+++ src/main/java/DB/JavaDB.java
@@ -0,0 +1,34 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+ 
+public class JavaDB {
+ 
+    public static void main(String[] args) {
+        // Nazwa bazy
+        String baza = "VehicleManager";
+        // Wywo³anie metody polacz, która zwraca obiekt typu Connection
+        Connection polaczenie = polacz(baza);   
+    }
+    
+    /**
+     * Metoda odpowiedzialna za po³¹czenie z baz¹
+     * jeœli bazy nie ma to zostaje utworzona
+     */
+    public static Connection polacz(String baza) {
+        Connection polaczenie = null;
+        try {
+            // Wskazanie jaki rodzaj bazy danych bêdzie wykorzystany, tu sqlite
+            Class.forName("org.sqlite.JDBC");           
+            // Po³¹czenie, wskazujemy rodzaj bazy i jej nazwê
+            polaczenie = DriverManager.getConnection("jdbc:sqlite:"+baza+".db");
+            System.out.println("Po³¹czy³em siê z baz¹ "+baza);
+        } catch (Exception e) {
+            System.err.println("B³¹d w po³¹czeniu z baz¹: \n" + e.getMessage());
+            return null;
+        }
+        return polaczenie;
+    }
+}
