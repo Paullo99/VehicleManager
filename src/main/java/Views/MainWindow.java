212a478f94af389f0755057d824eb58c5f4866f4
Paweł_Maciończyk <paulo99@poczta.onet.pl>
2019-06-05 14:14:02 +0200

    Adding info about program

--- src/main/java/Views/MainWindow.java
+++ src/main/java/Views/MainWindow.java
@@ -1,8 +1,14 @@
 package Views;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -31,11 +37,57 @@ public class MainWindow extends JFrame {
 	 */
 	public MainWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 826, 550);
+		setBounds(100, 100, 1162, 660);
+		setLocationRelativeTo(null);
 		contentPanel = new JPanel();
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPanel);
 		contentPanel.setLayout(null);
+		
+		JButton btnVehicles = new JButton("Pojazdy");
+		btnVehicles.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnVehicles.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnVehicles.setBounds(0, 0, 125, 125);
+		contentPanel.add(btnVehicles);
+		
+		JButton btnAddVehicle = new JButton("<html> Dodaj <br> pojazd </html>");
+		btnAddVehicle.setToolTipText("");
+		btnAddVehicle.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnAddVehicle.setBounds(0, 124, 125, 125);
+		contentPanel.add(btnAddVehicle);
+		
+		JButton btnStatistics = new JButton("Statystyki");
+		btnStatistics.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnStatistics.setBounds(0, 248, 125, 125);
+		contentPanel.add(btnStatistics);
+		
+		JButton btnHelp = new JButton("<html> Pomoc <br> (info)  </html>");
+		btnHelp.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnHelp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(null,"<html> Wersja programu: 1.0.0 <br>"
+							+ "Data powstania: __.06.2019 r. <br><br>"
+							+ "Autorzy: <br>"
+							+ "Kewin Warzecha <br>"
+							+ "Artur So³tys <br>"
+							+ "Pawe³ Macioñczyk <br>"
+							+ " </html>");
+			}
+		});
+		btnHelp.setBounds(0, 373, 125, 125);
+		contentPanel.add(btnHelp);
+		
+		JButton btnLogOut = new JButton("Wyloguj");
+		btnLogOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {			
+				
+			}
+		});
+		btnLogOut.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnLogOut.setBounds(0, 498, 125, 125);
+		contentPanel.add(btnLogOut);
 	}
-
 }
